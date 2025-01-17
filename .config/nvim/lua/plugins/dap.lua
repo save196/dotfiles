b0415256c3cb1cc0fc2122c4ab387f2c38577286
save196 <saverioturetta@protonmail.com>
2025-01-17 11:16:55 +0200

    nvim: added nvim-tree and dap plugins

---
+++ .config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,53 @@
+return {
+  'mfussenegger/nvim-dap',
+  dependencies = {
+    'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text',
+    'nvim-neotest/nvim-nio',
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+    'leoluz/nvim-dap-go',
+  },
+  config = function()
+    local dap = require 'dap'
+    local dapui = require 'dapui'
+
+    require('mason-nvim-dap').setup {
+      ensure_installed = { 'python', 'delve' },
+      handlers = {},
+    }
+    require('dapui').setup()
+    require('nvim-dap-virtual-text').setup {
+      display_callback = function(variable)
+        if #variable.value > 15 then
+          return ' = ' .. string.sub(variable.value, 1, 15) .. '... '
+        end
+        return ' = ' .. variable.value
+      end,
+    }
+    require('dap-go').setup()
+
+    -- Basic debugging keymaps, feel free to change to your liking!
+    vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+    vim.keymap.set('n', '<F6>', dap.run_to_cursor, { desc = 'Debug: Continue to Cursor' })
+    vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>B', function()
+      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+    end, { desc = 'Debug: Set Breakpoint' })
+
+    -- Eval var under cursor
+    vim.keymap.set("n", "<space>?", function()
+      dapui.eval(nil, { enter = true })
+    end)
+
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+  end,
+}
