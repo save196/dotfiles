a270f8521c22c436a253fb1b02e569c07610fefa
save196 <saverioturetta@protonmail.com>
2025-10-07 11:12:16 +0300

    nvim: changed lsp engine

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,26 +4,39 @@ return {
     dependencies = {
       { "williamboman/mason.nvim", config = true },
       "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "f3fora/cmp-spell",
-      "hrsh7th/nvim-cmp",
       { "j-hui/fidget.nvim",       opts = {} },
-      "rafamadriz/friendly-snippets",
-      { "garymjr/nvim-snippets", opts = { friendly_snippets = true } },
+      {
+        'saghen/blink.compat',
+        version = '*',
+        lazy = true,
+        opts = {},
+      },
+      {
+        "saghen/blink.cmp",
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = '*',
+        opts = {
+          completion = {
+            ghost_text = { enabled = false },
+          },
+          sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+          },
+          signature = { enabled = true },
+        },
+      },
     },
 
     config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
           "bashls",
           "clangd",
-          "pylsp"
+          "pyright",
+          "ruff"
         },
         handlers = {
           function(server_name) -- default handler (optional)
@@ -44,72 +57,31 @@ return {
               }
             }
           end,
-          ["pylsp"] = function()
+          ["pyright"] = function()
             local lspconfig = require("lspconfig")
-            lspconfig.pylsp.setup {
+            lspconfig.pyright.setup {
               capabilities = capabilities,
               settings = {
-                pylsp = {
-                  plugins = {
-                    pycodestyle = { maxLineLength = 88 },
-                  }
-                }
-              }
+                pyright = {
+                  -- Using Ruff's import organizer
+                  disableOrganizeImports = true,
+                },
+                python = {
+                  analysis = {
+                    -- Ignore all files for analysis to exclusively use Ruff for linting
+                    ignore = { '*' },
+                  },
+                },
+              },
+            }
+          end,
+          ["ruff"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.ruff.setup {
+              capabilities = capabilities,
             }
           end,
         }
-      })
-
-      local cmp = require('cmp')
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm(),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif vim.snippet.jumpable(1) then
-              vim.snippet.jump(1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif vim.snippet.jumpable(-1) then
-              vim.snippet.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'snippets' }
-        }, {
-          { name = 'buffer' },
-          { name = 'path' },
-          {
-            name = 'spell',
-            option = {
-              enable_in_context = function(_)
-                return require('cmp.config.context').in_treesitter_capture('spell') or vim.bo.filetype == 'vimwiki'
-              end,
-            },
-          },
-        }),
-        experimental = {
-          ghost_text = true,
-        },
       })
     end
   }
