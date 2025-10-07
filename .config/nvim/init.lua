e7c11fd80d6c43194412963b41a66aa08d445f54
save196 <saverioturetta@protonmail.com>
2025-10-07 10:38:49 +0300

    nvim: updated init to use lsp defaults

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,19 +21,20 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 -- Lsp mapping
+-- Defaults:
+-- "K" hover
+-- "grn" rename
+-- "gra" code actions
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(e)
     local bufopts = { buffer = e.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
     vim.keymap.set('n', '<space>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, bufopts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
   end
 })
@@ -43,6 +44,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
