63e5a7b8341e5866bef553af34bae4858bd5973b
save196 <saverioturetta@protonmail.com>
2026-04-05 14:36:55 +0300

    Update dwl autostart.sh

--- .local/share/dwl/autostart.sh
+++ .local/share/dwl/autostart.sh
@@ -3,12 +3,13 @@
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
-lock_cmd="swaylock -f -c 000000"
-
 pgrep dunst || dunst &
 pgrep udiskie || udiskie -qAT --no-appindicator &
-pgrep swayidle || swayidle -w timeout 300 'wlopm --off "*"' resume 'wlopm --on "*"' timeout 305 "$lock_cmd" timeout 3600 'systemctl suspend' before-sleep "$lock_cmd" &
+pkill -f swayidle-dbus; swayidle-dbus --lock-cmd "swaylock -f -c 000000" &
 pkill swaybg; swaybg -i ~/Pictures/landscape_81.jpg -m fill &
+pkill kanshy; kanshi &
+/usr/bin/nextcloud --background &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 # Update volume on statusbar
 pkill -RTMIN+10 dwlblocks
